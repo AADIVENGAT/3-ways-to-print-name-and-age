e0f2c68661d6f87e33a3f835baf00855a1eff018
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-01 20:05:43 +0530

    Add files via upload

---
+++ empty.py
@@ -0,0 +1,14 @@
+x="vedika"
+y=23
+print("Welcome "+x+" . Your age is "+str(y))
+
+
+x="vedika"
+y=23
+print("Welcome",x,"\b. Your age is ",y)
+
+x="vedika"
+y=23
+print("Welcome",x,". Your age is ",y)
+
+
